a5d6ef7f5b2fa8df50e1fa6dd420da1442fda7da
Crystal <crystal_brusch@brown.edu>
2020-06-26 13:41:42 -0400

    site regen for doi clipboard fix

--- assets/js/topnav.js
+++ assets/js/topnav.js
@@ -120,26 +120,30 @@ $(function () {
 		$(searchQueryInput).val('');
 	}
 
+	/* Copy DOI to Clipboard */
 	$(document).on('click', '[data-lightbox]', lity);
-
 	$('.btn--doi').click(function(e) {
 		var text = $(this).attr('data-copy');
 		var el = $(this);
 		copyToClipboard(text, el);
 	});
-
 	function copyToClipboard(text, el) {
-			var copyTest = document.queryCommandSupported('copy');
+		var copyTest = document.queryCommandSupported('copy');
 
 		if (copyTest === true) {
 				var copyTextArea = document.createElement("textarea");
+				copyTextArea.classList.add("doi-text");
 				copyTextArea.value = text;
 				document.body.appendChild(copyTextArea);
 				copyTextArea.select();
 				document.execCommand('copy');
+				removeDoiHack(copyTextArea);
 		} else {
 		// Fallback if browser doesn't support .execCommand('copy')
 			window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
 		}
+	}
+	function removeDoiHack(DOIhackTextArea) {
+		document.body.removeChild(DOIhackTextArea);
 	}
 });
